fb118a67e3e676fa748c887a78dea9f86a575773
wogha95 <wogha95@naver.com>
2025-01-25 12:24:16 +0900

    ch9: app.js, routes, controllers 작성

---
+++ practice-nodebird/app.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const nunjucks = require("nunjucks");
+const dotenv = require("dotenv");
+
+const pageRouter = require("./routes/page");
+const app = express();
+
+// .env 파일 설정
+dotenv.config();
+
+// port 설정
+app.set("port", process.env.PORT || 8001);
+
+// view 관련 파일 설정
+app.set("view engine", "html");
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
+});
+
+// logger 설정
+app.use(morgan("dev"));
+
+// 파싱 관련 설정
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// 쿠키, 세션 설정
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+
+// view 라우터 설정
+app.use("/", pageRouter);
+
+// 404 처리 미들웨어
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+// 에러 처리 미들웨어
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+// 서버 실행
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기중");
+});
