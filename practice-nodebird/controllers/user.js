7daa2710ab4bbe986a69dfdd6bbe8e7891c6c831
wogha95 <wogha95@naver.com>
2025-02-10 18:36:18 +0900

    ch9-self: 팔로잉 끊기

--- practice-nodebird/controllers/user.js
+++ practice-nodebird/controllers/user.js
@@ -1,13 +1,29 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
 exports.follow = async (req, res, next) => {
   try {
     const user = await User.findOne({ where: { id: req.user.id } });
-    if (user) { // req.user.id가 followerId, req.params.id가 followingId
+    if (user) {
+      // req.user.id가 followerId, req.params.id가 followingId
       await user.addFollowing(parseInt(req.params.id, 10));
-      res.send('success');
+      res.send("success");
     } else {
-      res.status(404).send('no user');
+      res.status(404).send("no user");
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+exports.unfollow = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { id: req.user.id } });
+    if (user) {
+      await user.removeFollowing(parseInt(req.params.id, 10));
+      res.send("success");
+    } else {
+      res.status(404).send("no user");
     }
   } catch (error) {
     console.error(error);
